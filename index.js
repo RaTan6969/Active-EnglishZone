ec5c3a31456f8eff181b694caf033d01a4540977
Nivetha <nive54@gmail.com>
2022-08-31 21:58:38 +0600

    second

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 
-// import express from "express";
 console.log("hi");
 const express =  require("express");
 const cors =  require("cors");
 const app = express();
 const data =  require("./Data.json");
+const sdata =  require("./Subdata.json");
 var port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send(data);
-    res.send(' world')
   })
   app.get('/data', (req, res) => {
-    res.send('hello world');
-    res.send(data);
-    console.log(data)
+    
+    res.send(sdata);
+
   })
 
 app.listen( port ,  () => {
